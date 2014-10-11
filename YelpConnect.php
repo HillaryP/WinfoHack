2257bbc8e2758fb5ea482e5bd4cc95551a328de4
HillaryP <prathh@uw.edu>
2014-10-11 14:02:52 -0700

    Create YelpConnect.php

---
+++ YelpConnect.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+		<script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
+	</head>
+	<body>
+		<h1>Washed Up</h1>
+		<form action="helper.php" method="post">
+			<input type="text" name="business" id="business" placeholder="Name">
+			<input type="text" name="loc" id="loc" placeholder="Location">
+			<input type="submit" value="Search">
+		</form>
+	</body>
+</html>
