7ac2d304971ba202ff9d018e1e9388a3313a5e30
Lily Dwyer <lmdwyer@uw.edu>
2014-10-11 17:51:17 -0700

    php

--- result.php
+++ result.php
@@ -1,64 +1,104 @@
+<!DOCTYPE html>
 <html>
-	<head>
-		<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-		<script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
-		<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-	</head>
-	<body>
-		<?php include 'helper.php' ?>
-		<h1>Washed-Up</h1>
-		<div id="info"></div>
+    <head lang="en">
+        <meta charset="UTF-8">
+        <title>Results</title>
+        <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/styles.css">
+         <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    </head>
+ 
+    <body>
+    	<?php include 'helper.php' ?>
+        <aside class="banner">
+            <img width="100" style="float:left; margin: 20px;" src="img/logo.png">
+            <p style = "font-size:20px; margin: 20px;">WASHED-UP</p>
+            <div class="text-vertical-center">
+                <p class="tagline">INTERACTIVE MAP</p>
+                <p class="heading3">Select a future time and check out the sea level. </p>
+            </div>
+        </aside>
 
-		<div class="dropdown">
-		    <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown">
-		        Dropdown
-		        <span class="caret"></span>
-		    </button>
-		    <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-		        <li role="presentation"><a role="menuitem" tabindex="1" click="make_map(0)">Action</a></li>
-		        <li role="presentation"><a role="menuitem" tabindex="2" click="make_map(1)">Another action</a></li>
-		        <li role="presentation"><a role="menuitem" tabindex="3" click="make_map(2)">Something else here</a></li>
-		        <li role="presentation"><a role="menuitem" tabindex="4" click="make_map(4)">Separated link</a></li>
-		        <li role="presentation"><a role="menuitem" tabindex="4" click="make_map(9)">Separated link</a></li>
-		        <li role="presentation"><a role="menuitem" tabindex="4" click="make_map(20)">Separated link</a></li>
-		    </ul>
-		</div>
+    <!-- dropdown menu -->
 
-		<iframe width="600" height="600" id="map">
-		</iframe>
+    <div class="dropdown">
+        <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown">
+            Dropdown
+            <span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu dropdown-styling" role="menu" aria-labelledby="dropdownMenu1">
+            <li role="presentation"><a role="menuitem" tabindex="1" href="#">50 years</a></li>
+            <li role="presentation"><a role="menuitem" tabindex="2" href="#">100 years</a></li>
+            <li role="presentation"><a role="menuitem" tabindex="3" href="#">200 years</a></li>
+            <li role="presentation"><a role="menuitem" tabindex="3" href="#">500 years</a></li>
+        </ul>
+    </div>
 
-		<?php
-			$term = $_POST['business'];
-			$location = $_POST['loc'];
 
-			$result = json_encode(query_api($term, $location));
-		?>
+        <?php
+            $term = $_POST['business'];
+            $location = $_POST['loc'];
 
-		<script type="text/javascript">
-		  $(document).ready(function() {
-		  	function make_map(meter) {
-		  		var php_result = JSON.parse(<?php echo $result; ?>);
-				var base = "http://flood.firetree.net/?ll=";
-			  	var lon = php_result.location.coordinate.longitude;
-			  	var comma = ",";
-			  	var lat = php_result.location.coordinate.latitude;
-			  	var and = "&";
-			  	var zequal = "z=";
-			  	var z = "1";
-			  	var mequal = "m=";
-			  	var m = "" + meter;
-			  	var type = "type=hybrid";
-			  	var url = base.concat(lat,comma,lon,and,zequal,z,and,mequal,meter,and,type);
-				$("#map").attr("src", url);
-			}
+            $result = json_encode(query_api($term, $location));
+            echo $result;
+        ?>
 
-			make_map(10);
-			$("#info").append(php_result.name);
-			$("#info").append(php_result.location.cross_streets);
-			$("#info").append("<img src=\"" + php_result.image_url + "\" />");
-		  });
-		</script>
-		
-	</body>
+        <script type="text/javascript">
+          $(document).ready(function() {
+                var php_result = JSON.parse(<?php echo $result; ?>);
+                var base = "http://flood.firetree.net/?ll=";
+                var lon = php_result.location.coordinate.longitude;
+                var comma = ",";
+                var lat = php_result.location.coordinate.latitude;
+                var and = "&";
+                var zequal = "z=";
+                var z = "1";
+                var mequal = "m=";
+                var m = "2";
+                var type = "type=hybrid";
+                var url = base.concat(lat,comma,lon,and,zequal,z,and,mequal,m,and,type)
+                $("#map").attr("src", url)
+                $("#info").append(php_result.name);
+                $("#info").append(php_result.location.cross_streets);
+                $("#info").append("<img src=\"" + php_result.image_url + "\" />");
+          });
+        </script>
+
+
+    <div class="container-fluid">
+      <div class="row-fluid">
+        <!-- column left & map --> 
+        <div class="span10">
+                <iframe class="results-map" width="600" height="600" id="map">
+                </iframe>
+        </div>
+
+
+        <!-- column right --> 
+    <div class="span2">
+            <!-- tip side -->
+            <div class="tips">
+                <p class="tagline">Prevention Tips</p>
+            </div>
+            <!-- tip end--> 
+        </div>
+      </div>
+
+    </div>
+
+
+    </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
